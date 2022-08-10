bafbe0922287b2b1c84decbe2113817cee1b7a64
William Herring <73984595+william-herring@users.noreply.github.com>
2022-08-10 18:32:21 +1000

    added logos and designs

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="description" content="Penpal is a site for learning languages by writing to real people." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
